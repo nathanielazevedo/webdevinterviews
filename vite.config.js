83803b071ffb73c94b228cbeb3d5a5272b2e0c4a
nateazevedo <nathanielazevedo@hotmail.com>
2023-12-09 16:41:54 -0500

    add global plugin

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,52 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    css: true,
-    setupFiles: './setup.js',
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+    },
   },
-  define: {
-    global: 'window',
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill(),
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser', // <-- Fix from above
+    },
   },
 })
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       // Node.js global to browser globalThis
+//       define: {
+//         global: 'globalThis',
+//       },
+//     },
+//   },
+// test: {
+//   globals: true,
+//   environment: 'jsdom',
+//   css: true,
+//   setupFiles: './setup.js',
+// },
+// define: {
+//   global: { cache: {} },
+// },
+// })
