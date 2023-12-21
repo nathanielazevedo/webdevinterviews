ab38899ade95e32acc78d1cb43982843f33358a2
nateazevedo <nathanielazevedo@hotmail.com>
2023-12-21 16:11:48 -0500

    fix api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // THIS FILE SHOULD NEVER BE IMPORTED DIRECTLY
 // IMPORT FROM AUTHCONTEXT
 
-const BASE_URL = !import.meta.env.DEV
+const BASE_URL = import.meta.env.DEV
   ? 'http://localhost:5000'
   : 'http://54.205.196.167'
 const DELAY = import.meta.env.DEV ? 0 : 0
