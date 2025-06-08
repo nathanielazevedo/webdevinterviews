6774a68adb030b5f50349824fc18c369712e57ba
nateazevedo <nathanielazevedo@hotmail.com>
2025-06-07 21:14:46 -0400

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import { StrictMode } from 'react'
-import './styles/index.css'
-import Router from './Router'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import "./styles/index.css";
+import Router from "./Router";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <Router />
-  </StrictMode>)
+  </StrictMode>
+);
--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import TopNav from "./RootTopNav";
 import { Divider } from "@mui/material";
+import { Analytics } from "@vercel/analytics/next";
 
 const Root = () => (
   <>
+    <Analytics />
     <TopNav />
     <Divider />
     <main className="main">
