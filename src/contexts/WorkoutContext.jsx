0a5644399c649aa138236565ef2a8b43219315d3
nateazevedo <nathanielazevedo@hotmail.com>
2025-06-07 21:05:59 -0400

    remove backend dependency

--- src/contexts/WorkoutContext.jsx
+++ src/contexts/WorkoutContext.jsx
@@ -1,23 +1,13 @@
-import { createContext, useState, useContext, useEffect } from "react";
+import { createContext, useState } from "react";
 import { useParams } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
-import { AuthContext } from "../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
+import workouts from "../pages/workouts/workouts.json";
 
 const WorkoutContext = createContext();
 
 const WorkoutProvider = ({ children }) => {
   const params = useParams();
-  const { data: workout, loading, error } = useFetch(`/workouts/${params.id}`);
   const [fromLocal, setFromLocal] = useState(false);
-  const navigate = useNavigate();
-
-  if (loading) return null;
-
-  // if (error && error.status == 403) {
-  //   navigate('/new-member')
-  //   return
-  // }
+  const workout = workouts.find((w) => w.id == params.id);
 
   return (
     <WorkoutContext.Provider value={{ workout, fromLocal, setFromLocal }}>
--- src/pages/workouts/Workouts.jsx
+++ src/pages/workouts/Workouts.jsx
@@ -1,57 +1,38 @@
-import useFetch from '../../hooks/useFetch'
-import Alert from '@mui/material/Alert'
-import Header from '../../components/Header'
-import Item from '../../components/Item'
+import Alert from "@mui/material/Alert";
+import Header from "../../components/Header";
+import Item from "../../components/Item";
+import workouts from "./workouts.json";
 
 const Workouts = () => {
-  const url = `/workouts`
-  const { data: workouts, loading, error } = useFetch(url)
+  const url = `/workouts`;
 
   const renderBodyContent = () => {
-    if (loading) {
-      return (
-        <div className='items-container'>
-          {[1, 2, 3, 4, 5].map((index) => (
-            <Item key={index} skeleton={true} />
-          ))}
-        </div>
-      )
-    }
-
-    if (error || !workouts) {
-      return 'Error. Refresh the page.'
-    }
-
-    if (workouts.length === 0) {
-      return 'No workouts found.'
-    }
-
     const sortedWorkouts = workouts.sort((a, b) =>
       a.difficulty.localeCompare(b.difficulty)
-    )
+    );
 
     return (
-      <div className='items-container'>
+      <div className="items-container">
         {sortedWorkouts.map((item) => (
-          <Item key={item.id} item={item} basePath={'/workouts/'} />
+          <Item key={item.id} item={item} basePath={"/workouts/"} />
         ))}
       </div>
-    )
-  }
+    );
+  };
 
   return (
-    <div className='fit-wrapper'>
+    <div className="fit-wrapper">
       <Header
-        title='Workouts'
-        subtext='A programmer is an athlete of the mind. These workouts will train your abilities with React, JavaScript, HTML, CSS and DSA.'
+        title="Workouts"
+        subtext="A programmer is an athlete of the mind. These workouts will train your abilities with React, JavaScript, HTML, CSS and DSA."
       />
-      <Alert severity='info' className='workout-alert'>
+      <Alert severity="info" className="workout-alert">
         There is no value in these workouts being mobile friendly. Therefore,
         there is currenly no intention to do so.
       </Alert>
       {renderBodyContent()}
     </div>
-  )
-}
+  );
+};
 
-export default Workouts
+export default Workouts;
